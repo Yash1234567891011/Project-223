99509767ba179340d6f8f7c23953d46902541f80
Yash1234567891011 <73171838+Yash1234567891011@users.noreply.github.com>
2023-05-10 16:45:00 +0530

    Add files via upload

---
+++ zip_word.py
@@ -0,0 +1,38 @@
+import zipfile
+import time
+
+folderpath=input('path to the file: ')
+zipf=zipfile.ZipFile(folderpath)
+global result
+result=0
+global tried
+tried=0
+c=0
+if not zipf:
+    print('The zipped file/folder is not password protected! You can successfully open it!')
+if(result==0):
+    print("Sorry, password not found. A total of "+str(c)+"+ possible combinations tried in "+ str(duration)+" seconds. Password is not of 4 characters.")
+else:
+    duration= endtime - starttime 
+    print("Congrulations!!! Password found after trying "+str(c)+" combinations in "+str(duration)+" seconds")
+
+for i in range(len(words)):
+    word=words[i]
+    password=word.encode('utf8').strip()
+    c=c+1
+    print(f'Trying to decode password by: '.format(word))
+    try:
+        with zipfile.ZipFile(folderpath,'r') as zf:
+            zf.extractall(pwd=password)
+            print("Success! The password is: "+word)
+            endtime= time.time()
+            result=1
+        break
+    except:
+        pass
+
+else:
+    starttime=time.time()
+    wordListFile=open("wordlist.txt","r",errors="ignore")
+    body=wordListFile.read().lower()
+    words=body.split("\n")               
